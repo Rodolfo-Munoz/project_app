bb9f906d0618e2ab5b0a781c85a06bf9ecdba8c6
Rodolfo Munoz <mr.verloc@gmail.com>
2021-03-20 22:28:33 +0000

    added the rest of the Tables, Order, Product, Area, ProductType and Supplier

--- roseleaf_app/models.py
+++ roseleaf_app/models.py
@@ -43,3 +43,50 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Area(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductType(models.Model):
+    product_type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.product_type
+
+
+class Product(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(Member, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.date)
+
+
+
+
+
