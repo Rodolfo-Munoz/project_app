375abe3fa9afc39086939f3f799f34188a87f05f
Rodolfo Munoz <mr.verloc@gmail.com>
2021-04-06 18:43:42 +0100

    login form

--- project_app/urls.py
+++ project_app/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from roseleaf_app import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('roseleaf_app.urls')),
+    path('', views.login, name='login'),
+    path('home/', views.home, name='home'),
 ]
--- roseleaf_app/views.py
+++ roseleaf_app/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'roseleaf_app/home.html', {})
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'roseleaf_app/login.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'roseleaf_app/login.html', {'form':AuthenticationForm(), 'error':'Username and password did not match'})
+        else:
+            return redirect('home')
+
